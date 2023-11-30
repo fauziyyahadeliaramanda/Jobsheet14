e428f4bcdf67eb82be8d5ec151d5e086fe0cf354
faujiiiahhh <framandaadelia@gmail.com>
2023-11-30 08:28:40 +0700

    Modif 1

--- Percobaan2.java
+++ Percobaan2.java
@@ -1,20 +1,31 @@
 package Rekrusif;
 import java.util.Scanner;
-    public class Percobaan2 {
-        static int hitungPangkat (int x, int y) {
-            if (y == 0) {
-                return (1);
-            } else {
-                return (x * hitungPangkat(x, y - 1));
-            }
+public class Percobaan2 {
+    static int hitungPangkat (int x, int y) {
+        if (y == 0) {
+            return 1;
+        } else {
+            return (x * hitungPangkat(x, y - 1));
         }
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-            int bilangan, pangkat;
-            System.out.print("Bilangan yang dihitung : ");
-            bilangan = sc.nextInt();
-            System.out.print("Pangkat : ");
-            pangkat = sc.nextInt();
-            System.out.println(hitungPangkat(bilangan, pangkat));
     }
+    static void cetakDeretPangkat(int x, int y){
+        if (y == 0){
+            System.out.println("1");
+        } else {
+            System.out.print(x + "x");
+            cetakDeretPangkat(x, y - 1);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int bilangan, pangkat;
+        System.out.print("Bilangan yang dihitung : ");
+        bilangan = sc.nextInt();
+        
+        System.out.print("Pangkat : ");
+        pangkat = sc.nextInt();
+        System.out.println("Hasil perpangkatan : " + hitungPangkat(bilangan, pangkat));
+        
+        cetakDeretPangkat(bilangan, pangkat);
+   }
 }
